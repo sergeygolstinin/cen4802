40a2f6adc132b1fe09214ac34515e8ddce2a5a79
sergeygolstinin <133604085+sergeygolstinin@users.noreply.github.com>
2023-05-28 18:52:20 -0400

    Add files via upload

--- src/module2/Fibonacci.java
+++ src/module2/Fibonacci.java
@@ -1,7 +1,17 @@
 package module2;
 
+
+/**
+ * This class is used to generate the nth term of the Fibonacci sequence.
+ */
 public class Fibonacci {
 
+	/**
+     * This method calculates the nth term of the Fibonacci sequence using recursion.
+     * 
+     * @param n the position in the Fibonacci sequence
+     * @return the nth term of the Fibonacci sequence
+     */
 	 public static int fibonacciRecursive(int n) {
 	        if (n <= 1) {
 	            return n;
@@ -9,7 +19,11 @@ public class Fibonacci {
 	            return fibonacciRecursive(n - 1) + fibonacciRecursive(n - 2);
 	        }
 	    }
-
+	 /**
+	     * This is the main method which makes use of the fibonacci method.
+	     * 
+	     * @param args Unused.
+	     */
 	public static void main(String[] args) {
 		int n = 10;
         int nthTerm = fibonacciRecursive(n);
